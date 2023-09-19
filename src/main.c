97e078d0a4c51ab8cf1c40b204c6b97f510fdaf6
Lentokone <44865524+Aik-10@users.noreply.github.com>
2023-09-19 19:08:04 +0300

    chore: Main.c recreated

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+#include "../includes/file_handler.h"
+#include "../includes/navigation.h"
+#define MAX_PERSON_AMOUNT 10
 
 int main() {
-    printf("ASDASDAKS");
+    struct person savedPersons[MAX_PERSON_AMOUNT];
+
+    int personsCount = loadTempFilePersons(savedPersons);
+    if ( personsCount == -1 ) {
+        return 1;
+    }
+
+    generalNavigationMenu();
 
     return 0;
 }
